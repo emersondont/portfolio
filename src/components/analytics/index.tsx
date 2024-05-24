35467abf3ab9d194aef5f50746377f578f4c9d12
Emerson Vieira <emersondont@gmail.com>
2024-05-24 01:58:42 -0300

    refactor: Add Google Analytics tracking code to the website

---
+++ src/components/analytics/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function Analytics() {
+  return (
+    <>
+      <script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+      />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+    
+        gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+        `,
+        }}
+      />
+    </>
+  )
+}

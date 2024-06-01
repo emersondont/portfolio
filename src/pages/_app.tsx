d870d42ab2a18b060e26746f21783e6a3a8bf4ce
Emerson Vieira <emersondont@gmail.com>
2024-06-01 19:29:16 -0300

    refactor: update page title and add meta description

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,8 +7,9 @@ export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<MenuProvider>
 			<Head>
-				<title>Emersondont - Desenvolvedor</title>
+				<title>Emerson - Desenvolvedor</title>
 				<link rel="icon" href="icon.png" sizes="any" />
+				<meta name="description" content="Olá! Sou o Emerson, formado em Ciência da Computação pela UFPel e Desenvolvedor Full Stack. Sinta-se à vontade para explorar meu portfólio e conhecer mais sobre os projetos que realizei!" />
 			</Head>
 			<Component {...pageProps} />
 		</MenuProvider>

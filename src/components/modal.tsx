aebd8fa38db1a8e19ef1ec30a044d366f9098fe8
Emerson Vieira <emersondont@gmail.com>
2024-10-03 22:09:13 -0300

    refactor: Update modal component styling and behavior

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -9,17 +9,19 @@ type ModalProps = {
 
 export default function Modal(props: ModalProps) {
   return (
-    <dialog ref={props.dialogRef}
-      className="w-80 backdrop:bg-black backdrop:bg-opacity-50 backdrop:backdrop-blur-md bg-blur backdrop-blur-3xl overflow-hidden rounded-xl cursor-default p-3"
+    <dialog
+      ref={props.dialogRef}
+      onClick={props.toggleDialog}
+      className="w-96 backdrop:bg-black backdrop:bg-opacity-30 backdrop:backdrop-blur-md bg-transparent hover:bg-transparent p-0 overflow-hidden rounded-xl"
     >
-      <div className="flex">
-        <button onClick={props.toggleDialog} className="z-10 cursor-pointer text-white mb-2 p-1 rounded-full outline-none">
+      <div onClick={e => e.stopPropagation()} className="bg-blur overflow-hidden rounded-xl cursor-default p-3 flex flex-col items-center">
+        <button onClick={props.toggleDialog} className="z-10 cursor-pointer text-white mb-2 p-1 rounded-full outline-none ml-auto">
           <AiOutlineClose size={24} />
         </button>
-      </div>
 
-      {props.children}
-      <div className="border-modal" />
+        {props.children}
+        <div className="border-modal" />
+      </div>
     </dialog>
   )
 }
